829ed1fd2eab9c4131b3804d48afe2a895336e8b
syisseo <syisseo@naver.com>
2021-12-11 13:03:07 +0900

    resize background img

--- vanilaJs-momentum/js/background.js
+++ vanilaJs-momentum/js/background.js
@@ -2,10 +2,14 @@ const images = [
     "0.jpg", "1.jpg", "2.jpg"
 ];
 
+
+
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
+
 bgImage.src = `img/${chosenImage}`;
 // 아직은 js에만 존재
-
+bgImage.style.width = "500px";
+bgImage.style.height = "400px";
 // html document에 추가
 document.body.appendChild(bgImage);
