5dc2b426c5e9b995391ca0057931497e52859488
syisseo <syisseo@naver.com>
2021-12-12 17:10:30 +0900

    #8.0 weather and geolocation

--- vanilaJs-momentum/js/background.js
+++ vanilaJs-momentum/js/background.js
@@ -3,13 +3,22 @@ const images = [
 ];
 
 
-
+const body = document.querySelector("body");
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
+
 bgImage.src = `img/${chosenImage}`;
+
+
 // 아직은 js에만 존재
 bgImage.style.width = "500px";
 bgImage.style.height = "400px";
 // html document에 추가
-document.body.appendChild(bgImage);
+
+// document.body.appendChild(bgImage);
+
+body.style.backgroundImage = "url('" + `${bgImage.src}` + "')";
+body.style.backgroundSize = "cover";
+body.style.backgroundRepeat = "no-repeat";
+body.style.height = "100%";
---
+++ vanilaJs-momentum/js/weather.js
@@ -0,0 +1,25 @@
+// navigator
+// 브라우저에서 위치 좌표를 준다.
+// getCurrentPosition()은 2개의 argument가 필요하다.
+// 1. 모든 것이 잘 실행 될 때
+// 2. 에러 발생 시 실행할 함수
+
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const WEATHER_KEY = "c114568219c12f6cf9d28982ecb48d7d";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_KEY}&unitx=metric`;
+    fetch(url).then((response) => response.json()).then((data) => {
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+    })
+}
+
+function onGeoError() {
+    alert("Can't find wyou. No weather for you.")
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
